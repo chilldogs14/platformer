d8ea4660e8f41c17c620fc3733e3c88911f579e0
chilldogs14 <100097061+chilldogs14@users.noreply.github.com>
2022-07-07 10:59:11 +1200

    zoom
    did zoom stuff

--- scenes/Camera2D.gd
+++ scenes/Camera2D.gd
@@ -1,5 +1,12 @@
 extends Camera2D
 
+var target = null
+var zoomed = false
+var center = Vector2.ZERO
 
 func _ready():
-	pass
+	center = get_viewport_rect().size/2
+
+func _process(delta):
+	if Input. is_action_just_pressed("zoom"):
+		pass
