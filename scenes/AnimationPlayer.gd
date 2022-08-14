35f6f41972c2c1f7114b0820b59d68b98675ac49
chilldogs14 <100097061+chilldogs14@users.noreply.github.com>
2022-08-15 09:44:29 +1200

    checkpoints
    did code for checkpoints

--- scenes/AnimationPlayer.gd
+++ scenes/AnimationPlayer.gd
@@ -1,7 +1,1 @@
 extends AnimationPlayer
-export (int) var follow_speed = 50
-func _ready():
-	$Animimationplayer.play("spin")
-	
-func _process(delta):
-	
--- scenes/Camera2D.gd
+++ scenes/Camera2D.gd
@@ -10,9 +10,17 @@ func _ready():
 
 func _process(delta):
 	if Input. is_action_just_pressed("zoom"):
-		if not zoomed:
-			target = center
+		if zoomed:
+			target = null
 			zoomed = false
 		else:
-			target = owner.get_node("player").global_position
+			target = owner.get_node("player")
 			zoomed = true
+	
+	if zoomed:
+		zoom = zoom.move_toward(Vector2(0.3,0.3), .03)
+		position = position.move_toward(target.global_position,80)
+	
+	else:
+		zoom = zoom.move_toward(Vector2(1,1),0.03)
+		position = position.move_toward(center,80)
